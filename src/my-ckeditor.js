7fb8d37e510782f4ceb6a1619a3b43ce6d8167ef
Aaron Surty <aaron.surty@gmail.com>
2013-05-30 12:24:10 -0400

    adding editable list

--- src/my-ckeditor.js
+++ src/my-ckeditor.js
@@ -1,6 +1,12 @@
 define(['ckeditor', 'jquery'], function() {
     $(function() {
         CKEDITOR.replace('ckeditor');
+
+        $('#ckeditor-container pre').click(function() {
+            var str = $(this).html().trim();
+            CKEDITOR.instances.ckeditor.insertText(str);
+        });  
+
     });
 
 });
