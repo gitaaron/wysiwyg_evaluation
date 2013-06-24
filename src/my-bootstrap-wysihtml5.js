029f71d609823524bacbeb31511c0cc76df3714f
Joel Mizzoni <jpmizzoni@gmail.com>
2013-06-24 12:18:06 -0400

    Added font size selector to wysihtml5 and font size plugin to CKEditor. Also cleaned up the CKEditor toolbar, removing some unnecessary buttons

--- src/my-bootstrap-wysihtml5.js
+++ src/my-bootstrap-wysihtml5.js
@@ -16,6 +16,30 @@ define(['exports', 'bootstrap-wysihtml5'], function(exports) {
                         }
                 });
             } else {
+		var myCustomTemplate = {
+			'font-styles' : function(locale) {
+                                 
+			return "<li class = dropdown>" +
+					"<a class='btn dropdown-toggle' data-toggle='dropdown'>"+
+						"<span class='current-size'>Font Size</span>" +
+						"<b class='caret'></b>"+
+					"</a>"+
+					"<ul class='dropdown-menu'>"+
+						"<li>"+
+                        				"<a class='wysihtml5-font-size' data-wysihtml5-command='fontSize' data-wysihtml5-command-value='large' title='font-size'>Large</a>" +
+						"</li>"+
+						"<li>"+ 
+							"<a class='wysihtml5-font-size' data-wysihtml5-command='fontSize' data-wysihtml5-command-value='medium' title='font-size'>Medium</a>"+
+						"</li>"+
+						"<li>"+ 
+							"<a class='wysihtml5-font-size' data-wysihtml5-command='fontSize' data-wysihtml5-command-value='small' title='font-size'>Small</a>"+
+						"</li>"+
+						
+					"</ul>"+
+                        "</li>";      	        
+			 }
+ 		}
+
                 $('#some-textarea').wysihtml5({
                         "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
                         "emphasis": true, //Italics, bold, etc. Default true
@@ -23,7 +47,35 @@ define(['exports', 'bootstrap-wysihtml5'], function(exports) {
                         "html": true, //Button which allows you to edit the generated HTML. Default false
                         "link": false, //Button to insert a link. Default true
                         "image": false, //Button to insert an image. Default true,
-                        "color": true //Button to change color of font  
+                        "color": true, //Button to change color of font, 
+			customTemplates: myCustomTemplate,
+			parserRules: {
+				classes: {
+					"wysiwyg-font-size-large":1,
+					"wysiwyg-font-size-medium":1,
+					"wysiwyg-font-size-small":1,
+					"wysiwyg-color-silver" : 1,
+                			"wysiwyg-color-gray" : 1,
+                			"wysiwyg-color-white" : 1,
+               	 			"wysiwyg-color-maroon" : 1,
+					"wysiwyg-color-red" : 1,
+					"wysiwyg-color-purple" : 1,
+					"wysiwyg-color-fuchsia" : 1,
+					"wysiwyg-color-green" : 1,
+					"wysiwyg-color-lime" : 1,
+					"wysiwyg-color-olive" : 1,
+					"wysiwyg-color-yellow" : 1,
+					"wysiwyg-color-navy" : 1,
+					"wysiwyg-color-blue" : 1,
+					"wysiwyg-color-teal" : 1,
+					"wysiwyg-color-aqua" : 1,
+					"wysiwyg-color-orange" : 1
+				},
+			
+				tags :{
+					"span":1
+				}
+			} 
                 });
 
             }
